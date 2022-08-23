92666b47f376cd91cb81ff8a70e0d3df5fc7e24e
IgorGreenIGM <107557907+IgorGreenIGM@users.noreply.github.com>
2022-08-23 10:58:00 +0100

    Update compile.bat

--- compile.bat
+++ compile.bat
@@ -1,6 +1,18 @@
 cd %cd%
 
-g++ "src/main.cpp" "src/ArgManager/argList.cpp" "src/ArgManager/treatment.cpp" "src/ArgManager/userinterface.cpp" "src/DrawingMethods/drawUtilities.cpp" "src/DrawingMethods/sorting.cpp" "src/DrawingMethods/streamRead.cpp" "src/DrawingMethods/plot.cpp" "src/StreamOperations/streamOperations.cpp" "src/DrawingMethods/drawStream.cpp" -o "./bin/output.exe" -L"./lib" -m32 -g -lopengl32 -lglut32
+g++^
+ "src/main.cpp"^
+ "src/ArgManager/argList.cpp"^
+ "src/ArgManager/treatment.cpp"^
+ "src/ArgManager/userinterface.cpp"^
+ "src/DrawingMethods/drawUtilities.cpp"^
+ "src/DrawingMethods/sorting.cpp"^
+ "src/DrawingMethods/streamRead.cpp"^
+ "src/DrawingMethods/plot.cpp"^
+ "src/StreamOperations/streamOperations.cpp"^
+ "src/DrawingMethods/drawStream.cpp"^
+ -o "./bin/output.exe"^
+ -L"./lib" -m32 -g -lopengl32 -lglut32
 
 @echo off
 
